2b295f1a4cc978d47e7c56fedabd996025031f7b
nieshankun <38554448+nieshankun@users.noreply.github.com>
2018-08-06 08:32:12 +0800

    delete hystrix dashboard

--- src/main/java/com/nsk/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
+++ src/main/java/com/nsk/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 @SpringBootApplication
 public class MicroserviceSimpleConsumerMovieApplication {
 
